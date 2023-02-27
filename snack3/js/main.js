85730a45c23db1b85e012e7d52b1be56ef8c8cc3
albertogioia93 <albertogioia@hotmail.it>
2023-02-27 12:10:55 +0100

    snack3

---
+++ snack3/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// JSSnack3
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+const lista = ['gino', 'paolo', 'franco'];
+
+lista [0] = 'ajeje';
+lista [1] = 'brazorf';
+lista [2] = 'ptaer';
+
+console.log(lista);
