4ae2ba96d3afe2b02bb51fdd0ecca0bf232ca0c3
albertogioia93 <albertogioia@hotmail.it>
2023-02-27 11:56:44 +0100

    snack2

--- snack2/js/main.js
+++ snack2/js/main.js
@@ -3,3 +3,11 @@
 // JSsnack2
 // Inserisci un numero, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
+
+const input = Number(prompt('inserisci un numero'));
+
+if (input % 0) {
+    console.log(input);
+} else {
+    console.log(input++);
+}
