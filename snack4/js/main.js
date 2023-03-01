49529e28fc06fbe470c3698aa35eeb33a35f931d
albertogioia93 <albertogioia@hotmail.it>
2023-03-01 11:07:55 +0100

    snack4

---
+++ snack4/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+
+// const numero = parseInt(prompt('inserisci un numero'));
+
+for (let i = 0; i < 5; i++){
+    const numero = parseInt(prompt('inserisci un numero'));
+}
